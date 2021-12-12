2cf63eb6d3f323d93395e3f370aa42eb375c5133
hakoai <hakoai64@gmail.com>
2021-12-13 02:21:31 +0900

    Update

--- config.fish
+++ config.fish
@@ -1,6 +1,3 @@
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
+if status is-interactive
+    # Commands to run in interactive sessions can go here
 end
-
---
+++ install-app.mjs
@@ -0,0 +1,48 @@
+#!/usr/bin/env zx
+
+
+// fish install
+if (await nothrow($`type fish`).exitCode !== 0) {
+    if (await nothrow($`[[ -f /etc/debian_version ]]`).exitCode === 0) {
+        await $`sudo apt-get update`
+        await $`sudo apt-get install software-properties-common -y`
+        await $`sudo apt-add-repository ppa:fish-shell/release-3`
+        await $`sudo apt-get update`
+        await $`sudo apt-get install fish -y`
+    }
+}
+
+// mosh install
+if (await nothrow($`type mosh`).exitCode !== 0) {
+    if (await nothrow($`[[ -f /etc/debian_version ]]`).exitCode === 0) {
+        await $`sudo apt-get update`
+        await $`sudo apt-get install mosh -y`
+    }
+}
+
+// tmux install
+if (await nothrow($`type tmux`).exitCode !== 0) {
+    if (await nothrow($`[[ -f /etc/debian_version ]]`).exitCode === 0) {
+        await $`sudo apt-get update`
+        await $`sudo apt-get install tmux -y`
+    }
+}
+
+// fisher install
+$.shell = '/usr/bin/fish'
+$.prefix = ''
+if (await nothrow($`type fihser`).exitCode !== 0) {
+    await $`curl -sL https://git.io/fisher | source && fisher update`
+}
+await $`fisher update`
+
+
+if (await nothrow($`type fnm`).exitCode !== 0) {
+    await $`curl -fsSL https://fnm.vercel.app/install | fish`
+}
+
+// fzf install
+if (await nothrow($`type fzf`).exitCode !== 0) {
+    await $`git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf`
+    await $`~/.fzf/install --all`
+}
