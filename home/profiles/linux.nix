ff1d4158fd10a177e762dcbc1a0021f64c8e81f1
hakoai <hakoai64@gmail.com>
2026-03-23 21:39:44 +0900

    bump

--- home/profiles/linux.nix
+++ home/profiles/linux.nix
@@ -7,8 +7,7 @@
     description = "Update flake and show home-manager diff with nvd";
     body = ''
       set -l dotfiles ~/dotfiles
-      nix flake update $dotfiles
-      and home-manager build --flake $dotfiles#linux
+      home-manager build --flake $dotfiles#linux
       and nvd diff ~/.local/state/nix/profiles/home-manager $dotfiles/result
     '';
   };
--- home/profiles/wsl.nix
+++ home/profiles/wsl.nix
@@ -7,8 +7,7 @@
     description = "Update flake and show home-manager diff with nvd";
     body = ''
       set -l dotfiles ~/dotfiles
-      nix flake update $dotfiles
-      and home-manager build --flake $dotfiles#wsl
+      home-manager build --flake $dotfiles#wsl
       and nvd diff ~/.local/state/nix/profiles/home-manager $dotfiles/result
     '';
   };
